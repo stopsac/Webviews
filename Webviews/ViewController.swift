349cdd05116c1a60c304f074ff552f8da7f59aca
Bono Kim <stopsac@naver.com>
2016-05-04 15:58:06 +0900

    first commit

--- Webviews/ViewController.swift
+++ Webviews/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.webView.delegate = self
+        
+        self.backButton.enabled = false
+        self.forwardButton.enabled = false
+        
+        if let url = NSURL(string: "http://www.oreilly.com") {
+            
+            let request = NSURLRequest(URL: url)
+            self.webView.loadRequest(request)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func goBack(sender: AnyObject) {
+        self.webView.goBack()
+    }
+    
+    @IBAction func goForward(sender: AnyObject) {
+        self.webView.goForward()
+    }
+    
+    @IBAction func refresh(sender: AnyObject) {
+        self.webView.reload()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.backButton.enabled = self.webView.canGoBack
+        self.forwardButton.enabled = self.webView.canGoForward
+        
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if navigationType == UIWebViewNavigationType.LinkClicked {
+            UIApplication.sharedApplication().openURL(request.URL!)
+            return false
+            
+        }
+        return true
+    }
+    
+    @IBAction func runJS(sender: AnyObject) {
+        self.webView.stringByEvaluatingJavaScriptFromString("alert('Hello')")
+    }
+    
 
 }
 
